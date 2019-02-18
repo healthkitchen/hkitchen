898b0af85bc6066552c4c91d73ea9d0c228c0d34
HarrisonPearl <Hspearl@gmail.com>
2019-02-17 22:56:00 -0600

    shopping.js loads my recipies

--- shopping.js
+++ shopping.js
@@ -1,3 +1,24 @@
-function loadShoppingList(){
+function loadShoppingList()
+{
+
+  console.log("Initializing Shopping List page");
+
+  var myrecipies = [recipes[0], recipes[0], recipes[0], recipes[0]];
+
+  for(var i = 0; i < myrecipies.length; i++)
+  {
+    var node = document.createElement("LI");
+
+    var t = document.querySelector("#cardtemplate");
+    t.content.querySelector('img').src = "images/science.png";
+    t.content.querySelector('.description').innerHTML = "delicious";
+    t.content.querySelector('a').href = "recipe.html#0"
+
+    var clone = document.importNode(t.content,true);
+
+    node.appendChild(clone);
+    document.getElementById("ingredients-list").appendChild(node);
+  }
+
 
 };
