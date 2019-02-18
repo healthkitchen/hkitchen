a783aa6acdc51149feb7456d1c369ce5e8b315a5
mhjasonliu <jasonliu2020.1@u.northwestern.edu>
2019-02-18 17:24:38 -0600

    data filter added

--- mainscript.js
+++ mainscript.js
@@ -1,11 +1,26 @@
+var name_query;
+var price_query;
 
-
-function init(){
+function init_index(){
     console.log("Initializing index page");
+
+    //clear out elements in content div and re-render
+    var node = document.querySelector('.row');
+    while (node && node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+
+    name_query = name_query || "";
+    price_query = price_query || Infinity;
 
     for(var i = 0; i < recipes.length; i++){
     var t = document.querySelector("#cardtemplate");
-    var r = recipes[i]
+    var r = recipes[i];
+
+    if(r.homeprice > price_query || !(r.name.toLowerCase().includes(name_query.toLowerCase()))){
+        continue;
+    }
+
     t.content.querySelector('img').src = r.imgsrc;
     t.content.querySelector('.description').innerHTML = r.name + " $" +r.homeprice;
     t.content.querySelector('a').href = "recipe.html#" + i;
@@ -13,4 +28,11 @@ function init(){
     var clone = document.importNode(t.content,true);
     document.querySelector(".cards > .row").appendChild(clone);
     }
+}
+
+function search(){
+    name_query = document.querySelector(".search-cards").value;
+    price_query = parseFloat(document.querySelector(".search-price").value)||Infinity;
+    console.log(price_query);
+    init_index();
 }
