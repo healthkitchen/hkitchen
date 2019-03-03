7daf5dab380d0995f1dcb491656ee43bc684d59e
Kevin Lee <kevin.son.lee@gmail.com>
2019-03-02 22:15:06 -0600

    resolving merge conflicts

--- recipe.js
+++ recipe.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*This function loads a recipe from a list of recipe objects in recipe_list.js */
 function loadRecipe(){
     //get the recipe you need
@@ -53,63 +52,4 @@ $(document).ready(function(){
     $('button').click(function(){
         $('.alert').show()
     }) 
-=======
-/*This function loads a recipe from a list of recipe objects in recipe_list.js */
-function loadRecipe(){
-    //get the recipe you need
-    console.log(window.location.href);
-    verifyUser();
-    var index = parseURI();
-    //index = recipes.length -1;
-    var recipe = recipes[index];
-    var homepricestring = String(recipe.homeprice.toFixed(2));
-    var retailpricestring = String(recipe.retailprice.toFixed(2));
-    var ingredients_list = document.getElementById("ingredients-list");
-    var directions_list = document.getElementById("directions-list");
-
-    document.getElementById("current_image").src = recipe.imgsrc;
-    document.getElementById("recipe-name").innerHTML=recipe.name;
-    document.getElementById("prices").innerHTML="Home Price: <font color='green'>$" + homepricestring
-     + "</font><br />Retail: <font color='red'>$" + retailpricestring 
-     + "</font><br />Cook Time: " + recipe.time
-     +"<br />" + recipe.yield;
-
-    for(var i of recipe.ingredients){
-        //console.log(i);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(i[0]));
-        ingredients_list.appendChild(li);
-    }
-    for(var d of recipe.directions){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(d));
-        directions_list.appendChild(li);
-    }
-}
-
-//get current recipe index from URI
-function parseURI(){
-    var uri = decodeURI(window.location.href);
-    var loc = uri.indexOf('#') + 1;
-    return parseInt(uri.substring(loc));
-}
-
-
-
-
-function addRec()
-{
-  var retrievedmyrecipes = localStorage.getItem("MR");
-  var myrecipes = JSON.parse(retrievedmyrecipes || '[]'); //if recipes doesnt exist, use empty array
-  var index = parseURI();
-  if(!myrecipes.includes(index)){
-      myrecipes.push(index);}
-  localStorage.setItem("MR", JSON.stringify(myrecipes));
-}
-
-$(document).ready(function(){
-    $('button').click(function(){
-        $('.alert').show()
-    }) 
->>>>>>> e68fc44c648d371b967628099a792c75a3e0f856
 });
